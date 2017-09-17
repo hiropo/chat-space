2d0d0d186d49d7b847526eee3b5724dab43649de
hiropo <szkhrt65@gmail.com>
2017-09-17 15:02:02 +0900

    replace to prefix

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,7 +13,7 @@ class GroupsController < ApplicationController
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
-      render :new
+      render new_group_path
     end
   end
 
