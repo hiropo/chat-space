8faf23fa97b357e4f414f4dba7396b60d0a1578d
hiropo <szkhrt65@gmail.com>
2017-09-17 16:15:05 +0900

    nest for group

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :edit, :update, :new, :create]
+  resources :groups, only: [:index, :edit, :update, :new, :create] do
+    resources :messages
+  end
 end
