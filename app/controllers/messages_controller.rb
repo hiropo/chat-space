c70aab55c61d01c59c862b5c76095f7c8ed026ef
hiropo <szkhrt65@gmail.com>
2017-10-05 00:10:14 +0900

    resolve the N+1 select problem

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,16 @@
 class MessagesController < ApplicationController
-  before_action :get_groups, only: [:index, :create]
-  before_action :set_group, only: [:index, :create]
+  before_action :get_groups
+  before_action :set_group
 
   def index
     @message = Message.new
+    @messages = @group.messages.includes(:user)
+
   end
 
   def create
     @message = Message.new(message_params)
+    @messages = @group.messages.includes(:user)
     if @message.save
       redirect_to group_messages_path, notice: "メッセージを作成しました。"
     else 
@@ -27,6 +30,9 @@ class MessagesController < ApplicationController
 
   def set_group
     @group = Group.find(params[:group_id])
-    @messages = @group.messages
+  end
+
+  def set_messages
+    @messages = @group.messages.includes(:user)
   end
 end
