f161626797c334952579349767ddab24789dfba7
hiropo <szkhrt65@gmail.com>
2017-10-08 01:14:15 +0900

    test for index action

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -2,33 +2,42 @@ require 'rails_helper'
 
 describe MessagesController, type: :controller do
   let(:user) { create(:user) }
-  # userをcreateし、let内に格納
+  let(:group) { create(:group) }
+  let(:messages) { create_list(:message, 5, user: user, group: group) }
 
     describe 'GET #index' do
-      before do
-        login_user user
-        # controller_macros.rb内のlogin_userメソッドを呼び出し
-      end
+      context 'when user is logged in' do
+        before do
+          login_user user
+          get :index, group_id: group 
+        end
+   
+        it "assigns the requested group to @group" do
+          expect(assigns(:group)).to eq(group)
+        end
+           
+        it "assigns the requested group to @messages" do
+          expect(assigns(:messages)).to match(messages)       
+        end
 
-      it "renders the :index template" do
-        get :index
-        expect(response).to render_template :index        
-      end
+        it "assigns the requested contact to @groups" do
+          groups = user.groups
+          expect(assigns(:groups)).to match groups
+        end    
+
+        it "renders the :index template"do
+          expect(response).to render_template :index
+        end  
+      end  
+
+      context 'when user is logged out' do
+        it 'redirects to login page'do
+          get :index, group_id: group 
+          expect(response).to redirect_to new_user_session_path
+        end
+      end  
   end
+  describe 'POST #create' do
+
+　end
 end
-
-
-# describe MessagesController do
-#   describe 'GET #index' do
-#     it "assigns the requested tweet to @group" do
-#       # group = create (:group)
-#      #  get :index , id: group
-#     end    
-
-#   end
-
-#   describe 'POST #create' do
-#     it "renders the :create template" do
-#     end
-#   end  
-# end
