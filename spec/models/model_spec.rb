66ce8e10cb60bb517cf4e5a7b8528f6251a21b51
hiropo <szkhrt65@gmail.com>
2017-10-07 16:32:07 +0900

    test for message model

--- spec/models/model_spec.rb
+++ spec/models/model_spec.rb
@@ -1,9 +1,43 @@
 require 'rails_helper'
+
 describe Message do
   describe '#create' do
-    it "is valid with a message" do
-      message = build(:message, body:"aaaaa")
-      expect(message).to be_valid
+    context 'enbale to save messages' do
+      it "is valid with a message" do
+        message = build(:message, image: '')
+        expect(message).to be_valid
+      end
+
+      it "is valid with a image" do
+        message = build(:message, body: '')
+        expect(message).to be_valid
+      end
+
+      it "is valid with a image and message" do
+        message = build(:message)
+        expect(message).to be_valid
+      end
     end
+
+    context 'unable to save messages'do
+      it "is invalid without a image and message" do
+        message = build(:message, body: '', image: '')
+        message.valid?
+        expect(message).not_to be_valid
+      end
+
+      it "is invalid without a group_id" do
+        message = build(:message, group_id: '')
+        message.valid?
+        expect(message).not_to be_valid
+      end 
+
+      it "is invalid without a user_id" do
+        message = build(:message, user_id: '')
+        message.valid?
+        expect(message).not_to be_valid
+      end  
+    end
+    
   end
 end
