6211fea558f6d94c7f450d6d8b09b94699177146
hiropo <szkhrt65@gmail.com>
2017-10-08 11:48:37 +0900

    delete admin statement

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,9 +1,4 @@
 module ControllerMacros
-  def login_admin(admin)
-    @request.env["devise.mapping"] = Devise.mappings[:admin]
-    sign_in admin
-  end
-
   def login_user(user)
     allow(controller).to receive(:current_user).and_return(user)
     @request.env["devise.mapping"] = Devise.mappings[:user]
