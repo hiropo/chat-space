0499d1363418c409a33262de835babed2df10c83
hiropo <szkhrt65@gmail.com>
2017-10-12 23:07:50 +0900

    auto moving for new meesage

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,20 @@
 $(function(){
+  function buildHTML(message){
+    var html = `<div class = "chat__messages__output-upper">
+                  <div class = "chat__messages__output-upper-name">
+                    <p>${message.name}</p>
+                  </div>
+                  <div class = "chat__messages__output-upper-date">
+                    <p>${message.created_at}</p>
+                  </div>
+                </div>
+                <div class = "chat__messages__output-lower">
+                  <div class = "chat__messages__output-lower-message">
+                    <p>${message.body}</p>
+                  </div>
+                </div>`
+    return html;
+  }  
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     console.log(this)
@@ -11,7 +27,15 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
-  })
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat__messages').append(html)
+      $('.chat__form__message').val('')
+      $('.chat__messages').animate( {scrollTop: $('.chat__messages__output')[0].scrollHeight} );
+    })
+    .fail(function(){
+      alert('error');
+    })    
 })
 })
-  
